0c4486ad354a00670d3d33e788a2bea73ce4864e
EuniceNam <caellime@gmail.com>
2026-04-30 18:31:43 +0900

    test: 13 red

--- src/test/java/com/back/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/back/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -10,6 +10,42 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class WiseSayingControllerTest {
     @Test
+    @DisplayName("13: 검색")
+    void t13() {
+        final String out = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                과거에 집착하지 마라.
+                작자미상
+                목록?keywordType=content&keyword=과거
+                종료
+                """);
+
+        assertThat(out)
+                .isEqualTo("""
+                        == 명언 앱 ==
+                        명령) 명언 : 작가 : 1번 명언이 등록되었습니다.
+                        명령) 명언 : 작가 : 2번 명언이 등록되었습니다.
+                        명령) ----------------------
+                        검색타입 : content
+                        검색어 : 과거
+                        ----------------------
+                        번호 / 작가 / 명언
+                        ----------------------
+                        2 / 작자미상 / 과거에 집착하지 마라.
+                        명령) ----------------------
+                        검색타입 : author
+                        검색어 : 작자
+                        ----------------------
+                        번호 / 작가 / 명언
+                        ----------------------
+                        2 / 작자미상 / 과거에 집착하지 마라.
+                        1 / 작자미상 / 현재를 사랑하라.
+                        명령)\s""");
+    }
+    @Test
     @DisplayName("3: 등록")
     void t3() {
         final String out = AppTestRunner.run("""
